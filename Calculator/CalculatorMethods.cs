882290baf17d5be854452ab61daae01dcc1e39ca
USER-PC\User <yury.ivanou1995@gmail.com>
2020-11-01 20:12:53 +0300

    Initial repository

---
+++ Calculator/CalculatorMethods.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading;
+
+namespace Project
+{
+    class CalculatorMethods
+    {        
+        internal double GetSecondNumber()
+        {
+            Console.WriteLine("Type second number and press Enter button: ");
+            double number2;
+            try
+            {
+                number2 = double.Parse(Console.ReadLine());
+                return number2;
+            }
+            catch (Exception)
+            {
+                throw new Exception("Number in incorrect format entered, please try again");
+            }
+        }
+        internal double Add(double x, double y)
+        {
+            if ((-9223372036854775808 < (x + y)) && ((x + y) < 9223372036854775807))
+            { return x + y; }
+            else
+            { throw new Exception("Result is outside the minimum / maximum values"); }            
+        }
+        internal double Subtract(double x, double y)
+        {
+            if (-9223372036854775808 < (x - y) && (x - y) < 9223372036854775807)
+            { return x - y; }
+            else
+            { throw new Exception("Result is outside the minimum / maximum values"); }
+        }
+        internal double Multiply(double x, double y)
+        {
+            if (-9223372036854775808 < (x * y) && (x * y) < 9223372036854775807)
+            { return x * y; }
+            else
+            { throw new Exception("Result is outside the minimum / maximum values"); }
+        }
+        internal double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new Exception("Cannot be divided by zero, please try again");
+            }
+            else if (-9223372036854775808 < (x / y) && (x / y) < 9223372036854775807)
+            { return x / y; }
+            else
+            { throw new Exception("Result is outside the minimum / maximum values"); }
+        }
+        internal double Elevate(double x, double y)
+        {
+            if (x < 0 && y != 0 && y > -1 && y < 1)
+            { throw new Exception("Invalid input, please try again"); }
+            else if (-9223372036854775808 < Math.Pow(x, y) && Math.Pow(x, y) < 9223372036854775807)
+            { return Math.Pow(x, y); }
+            else
+            { throw new Exception("Result is outside the minimum / maximum values"); }
+        }
+        internal long Parse(double x)
+        {
+            if (x > 0 && x % 1 == 0)
+            {
+                long res = checked((long)x);
+                return res;
+            }
+            else
+            {
+                throw new Exception("Cannot calculate the factorial of a negative/fractional/too large number, please try again");
+            }
+        }        
+        internal long Factorial(long res)
+        {            
+                if (res == 1)
+                {
+                    return 1;
+                }
+                return res * Factorial(res - 1);         
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CalculatorMethods method = new CalculatorMethods();
+
+            for (; ; )
+            {
+                try
+                {
+                    Console.WriteLine("\n\nType first number and press Enter button: ");
+                    double number1;
+                    try
+                    {
+                        number1 = double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception)
+                    {
+                        throw new Exception("Number in incorrect format entered, please try again");                        
+                    }
+
+                    Console.WriteLine("Type one of the available actions:  + - * / ^ ! and press Enter: ");
+                    var action = Console.ReadLine();
+
+                    switch (action)
+                    {
+                        case "+":
+                            Console.WriteLine("Result: \n" + method.Add(number1, method.GetSecondNumber()) + "\n");
+                            break;
+                        case "-":
+                            Console.WriteLine("Result: \n" + method.Subtract(number1, method.GetSecondNumber()) + "\n");
+                            break;
+                        case "*":
+                            Console.WriteLine("Result: \n" + method.Multiply(number1, method.GetSecondNumber()) + "\n");
+                            break;
+                        case "/":
+                            Console.WriteLine("Result: \n" + method.Divide(number1, method.GetSecondNumber()) + "\n");
+                            break;
+                        case "^":
+                            Console.WriteLine("Result: \n" + method.Elevate(number1, method.GetSecondNumber()) + "\n");
+                            break;
+                        case "!":
+                            if (number1 == 0)
+                            {
+                                Console.WriteLine("Result: \n1");
+                                break;
+                            }
+                            else if (number1 <= 65 && method.Factorial(method.Parse(number1)) != 0)
+                            {
+                                Console.WriteLine("Result: \n" + method.Factorial(method.Parse(number1)) + "\n");
+                                break;
+                            }                            
+                            else
+                            {
+                                Console.WriteLine("\nNumber is too large to calculate factorial, please try again");
+                                break;
+                            }                            
+                        default:
+                            throw new Exception("Incorrect operation entered, please try again");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"\n{e.Message}");
+                }
+
+                Console.WriteLine("\nIf you want to exit, type \"exit\" and press Enter. If you want to continue press Enter: ");
+                var ShouldContinue = Console.ReadLine();
+                if (ShouldContinue == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
