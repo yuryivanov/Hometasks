99333f02175668ce18eecd4573a1af2c934d7e0e
USER-PC\User <yury.ivanou1995@gmail.com>
2020-11-19 01:22:43 +0300

    Added class Airline

---
+++ Airline/Airline/Airline.cs
@@ -0,0 +1,8 @@
+﻿namespace AirlineNamespace
+{
+    public abstract class Airline
+    {
+        public static string CompanyName { get; set; }
+        static Airline() => CompanyName = "Yury's Airline Company";
+    }
+}
